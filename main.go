d10375c54de3f990850bf3b2e16a3c11f32560a8
Bui Quoc Trong <trong.buiquoc@gmail.com>
2020-09-05 18:37:33 +0700

    Move server setup into separate file, add auth middleware

--- main.go
+++ main.go
@@ -1,68 +1,15 @@
 package main
 
 import (
-  "net/http"
-  "log"
-  "strings"
-  "encoding/json"
   "snowflake-server/snowflake"
 )
 
 
-const AuthorizationHeaderPrefix = "Bearer"
-
-var idWorker *snowflake.IDWorker
-
-func init() {
-    var err error
-    idWorker, err = snowflake.NewIDWorker(1)
-    if err != nil {
-        log.Fatal(err)
-    }
-}
-
 func main() {
-    mux := http.NewServeMux()
-    mux.Handle("/ping", http.HandlerFunc(ping))
-    mux.Handle("/nextid", authMiddleware(http.HandlerFunc(nextID)))
-    mux.Handle("/stats", authMiddleware(http.HandlerFunc(stats)))
-
-    log.Println("Listening on :8000...")
-    err := http.ListenAndServe(":8000", mux)
+    server, err := snowflake.NewServer(1)
     if err != nil {
-        log.Fatal(err)
+        panic(err)
     }
+    server.Start()
 }
 
-func ping(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Pong"))
-}
-
-func nextID(w http.ResponseWriter, r *http.Request) {
-    id := idWorker.NextID()
-
-    idResp := struct {
-        ID int64 `json:"id"`
-    } { id }
-
-    data, err := json.Marshal(idResp)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
-
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(data)
-}
-
-func stats(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Not Implemented"))
-}
-
-func authMiddleware(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        auth := r.Header.Get("Authorization")
-        if !strings.HasPrefix(auth, AuthorizationHeaderPrefix) {
-        }
-        next.ServeHTTP(w, r)
-    })
-}
---
+++ snowflake/server.go
@@ -0,0 +1,96 @@
+package snowflake
+
+
+import (
+    "os"
+    "net/http"
+    "log"
+    "strings"
+    "encoding/json"
+)
+
+
+const AuthorizationHeaderPrefix = "Bearer"
+
+var APIKey string
+
+func init() {
+    APIKey = os.Getenv("API_KEY")
+    if len(APIKey) == 0 {
+        panic("API Key is missing!")
+    }
+}
+
+type Server struct {
+    idWorker *IDWorker
+}
+
+func NewServer(mID int) (*Server, error) {
+    idWorker, err := NewIDWorker(mID)
+    if err != nil {
+        return nil, err
+    }
+
+    s := Server { idWorker }
+
+    return &s, nil
+}
+
+func (s *Server) Start() {
+    mux := http.NewServeMux()
+    mux.Handle("/ping", http.HandlerFunc(ping))
+    mux.Handle("/nextid", authMiddleware(http.HandlerFunc(s.nextID)))
+    mux.Handle("/stats", authMiddleware(http.HandlerFunc(stats)))
+
+    log.Println("Listening on :8000...")
+    err := http.ListenAndServe(":8000", mux)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+func (s *Server) nextID(w http.ResponseWriter, r *http.Request) {
+    id := s.idWorker.NextID()
+
+    idResp := struct {
+        ID int64 `json:"id"`
+    } { id }
+
+    data, err := json.Marshal(idResp)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(data)
+}
+
+func ping(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Pong"))
+}
+
+
+func stats(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Not Implemented"))
+}
+
+func authMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        auth := strings.TrimSpace(r.Header.Get("Authorization"))
+        if len(auth) == 0 {
+            http.Error(w, "Authorization header is required", http.StatusUnauthorized)
+            return
+        }
+        if !strings.HasPrefix(auth, AuthorizationHeaderPrefix) {
+            http.Error(w, "Invalid Authorization header format", http.StatusUnauthorized)
+            return
+        }
+        tokens := strings.Split(auth, " ")
+        if len(tokens) != 2 || tokens[1] != APIKey {
+            http.Error(w, "API Key is incorrect", http.StatusUnauthorized)
+            return
+        }
+        next.ServeHTTP(w, r)
+    })
+}
